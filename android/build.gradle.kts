ac55160c3f7b50803bf892e2ebcf0ee6e339fae2
Sven Paulsen <info@sven-paulsen.de>
2026-09-03 14:26:42 +0200

    bump jackson-databind and firebase-bom to the upstream versions

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -60,9 +60,9 @@ dependencies {
     implementation("androidx.core:core-ktx:1.17.0")
     implementation("androidx.appcompat:appcompat:1.7.1")
     implementation("com.google.android.material:material:1.13.0")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.20.1")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.22.2")
 
-    implementation(platform("com.google.firebase:firebase-bom:34.7.0"))
+    implementation(platform("com.google.firebase:firebase-bom:34.18.0"))
     implementation("com.google.firebase:firebase-messaging-ktx:24.1.2")
     testImplementation("junit:junit:4.13.2")
     testImplementation("io.mockk:mockk-android:1.14.9")
